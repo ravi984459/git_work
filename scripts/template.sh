6ed09f20edf7dfdd73ea40092a0237e196e002cd
laxmireddy <laxmireddy7731@gmail.com>
2018-01-18 19:30:15 +0530

    This is template for script

---
+++ scripts/template.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+########################################
+# perpose: To learn how to print message
+# version: 1.0
+# owner: info@mycompany.com
+# input: message
+# output: print message
+#######################################
